0d39e7867f79a414d634d963bcca159bd3bfa9bd
green-beep <80179136+green-beep@users.noreply.github.com>
2021-03-06 18:34:10 -0500

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,10 +3,10 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' beep';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
+const gree1 = 'Go to Sleep!!!  ';
 const gree2 = 'Good morning!  ';
 const gree3 = 'Good afternoon  ';
 const gree4 = 'Good evening,  ';
@@ -15,7 +15,7 @@ const gree6 = 'Good evening,  ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = name + gree1;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText = gree2 + name;
 } else if (hour >= 12 && hour < 17) {
