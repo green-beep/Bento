2fd088e3f89029afe0abbdc65c46221e4a30cc55
green-beep <80179136+green-beep@users.noreply.github.com>
2021-03-06 18:43:50 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = 'de1ad6459f408d21aebf6c20873272e9';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 40.609200;
+  let longitude = -73.817960;
 
   getWeather(latitude, longitude);
 }
